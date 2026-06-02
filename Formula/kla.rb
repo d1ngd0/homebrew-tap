23ffb2706bc9f4fc04fa1f4e1bc36e2606975073
Paul Montag <paul.david.montag@gmail.com>
2026-06-01 23:15:32 -0500

    just work already

--- Formula/kla.rb
+++ Formula/kla.rb
@@ -4,10 +4,10 @@ class Kla < Formula
   version "0.4.2"
 
   if Hardware::CPU.arm?
-    url "https://github.com/d1ngd0/kla/releases/download/0.4.2/kla-aarch64-apple-darwin"
+    url "https://github.com/d1ngd0/kla/releases/download/0.4.2/kla-aarch64-apple-0.4.2"
     sha256 "4a922a00bd6cdf58e8cbc997270c2cb68f0e2a1dfa0c48768fa1456214d290ed"
   else
-    url "https://github.com/d1ngd0/kla/releases/download/0.4.2/kla-x86_64-apple-darwin"
+    url "https://github.com/d1ngd0/kla/releases/download/0.4.2/kla-x86_64-apple-0.4.2"
     sha256 "3d0f25993a3af7a8df6924a39309b83581a31752d47e71da433c57d14691e77a"
   end
 
