1f0e771b998df377fc0bcb8466cdfa91da0147b0
Paul Montag <paul.david.montag@gmail.com>
2026-07-29 21:09:49 -0500

    upgrade

--- Formula/kla.rb
+++ Formula/kla.rb
@@ -2,16 +2,16 @@ class Kla < Formula
   desc "An HTTP CLI application for working with restful endpoints"
   homepage "https://github.com/d1ngd0/kla"
   sha256 "04d1aeb9b556a42aa2ed5344860fcde161b687c1a8490aa65b56fb40bd1f1eea"
-  version "0.5.0"
+  version "0.5.1"
   url "https://github.com/d1ngd0/kla/releases/download/#{version}/kla-aarch64-apple-#{version}"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/d1ngd0/kla/releases/download/#{version}/kla-aarch64-apple-#{version}"
-      sha256 "4ed4c44cee3c7e5cfbbe4c08f4bd82f0c62c7583c8951d478d0029d25b799811"
+      sha256 "b4af08cd7726d01e084e272e311a01d61051093d0446aff77d01516dedf2558b"
     else
       url "https://github.com/d1ngd0/kla/releases/download/#{version}/kla-x86_64-apple-#{version}"
-      sha256 "de52f8f68bb924b0e1d2a6db5e93a7e3e134f2bcdadd886f787f2aa48675f278"
+      sha256 "8d8b5ce33f818978057a04f14072b32f98acb9ffc9375a815d1e15e96054c006"
     end
   end
 
